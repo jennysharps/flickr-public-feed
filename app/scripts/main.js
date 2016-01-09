97260ef52b325619aef7e89aa679e42e3aa72ce9
Jenny Sharps <jennylynnsharps@gmail.com>
2016-01-09 12:10:15 -0500

    Add LESS styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,10 +7,11 @@ require.config({
 });
 
 require([
+	'jquery',
 	'views/app',
 	'models/flickr-photo',
 	'collections/flickr-photos'
-], function(AppView, FlickrPhotoModel, FlickrPhotoCollection) {
+], function($, AppView, FlickrPhotoModel, FlickrPhotoCollection) {
 	new AppView;
 	var photo = new FlickrPhotoModel({'name': "photo1"});
 	console.log(photo.get('name'));
@@ -20,11 +21,25 @@ require([
 	window.glob = photo;
 
 	var photoFeed = new FlickrPhotoCollection();
+
+	var $main = $('#main'),
+		$ul = $main.append('<ul>');
+
+	photoFeed.on('add', updateView);
+
 	photoFeed.fetch({
 		data: {
 			tagmode: 'all',
-               tags: 'potato'
+            tags: 'potato'
         }
     });
 	window.feed = photoFeed;
+
+	function updateView(item) {
+		var $template = $('<li>');
+		$template.append('<a href="' + item.get('link') + '"><img src="' + item.get('imgsrc') + '">');
+		$template.append('<h1><a href="/detail/' + item.get('id') + '">' + item.get('title') + '</a></h1>');
+		$template.append('<a href="' + item.get('author').get('id') + '">' + item.get('author').get('name') + '</a>');
+		$ul.append($template);
+	}
 });
--- app/scripts/models/flickr-photo.js
+++ app/scripts/models/flickr-photo.js
@@ -5,10 +5,11 @@ define([
     var model = {};
 
     model.defaults = {
+        id: null,
         title: null,
         link: null,
-        imgSrc: null,
-        publishDate: null,
+        imgsrc: null,
+        published: null,
         author: new AuthorModel(),
         tags: []
     };
@@ -21,21 +22,31 @@ define([
         var attrs = {};
         res = res || {};
 
+        attrs.id = res.link.substring(res.link.replace(/\/$/, '').lastIndexOf('/') + 1);
         attrs.title = res.title;
         attrs.link = res.link;
-        attrs.imgSrc = res.media ? res.media.m : undefined;
+        attrs.imgsrc = res.media ? res.media.m : undefined;
+        attrs.published = res.published;
 
         attrs.author = new AuthorModel({
             name: res.author,
             id: res.author_id
         });
 
-        attrs.tags = [];
-        _.each(res.tags.split(" "), function(tag) {
-            attrs.tags.push(tag);
-        });
+        if(res.tags) {
+            attrs.tags = [];
+            _.each(res.tags.split(" "), function(tag) {
+                attrs.tags.push(tag);
+            });
+        }
 
         return attrs;
+    };
+
+    model.validate = function(attrs, options) {
+        if(attrs.published && typeof attrs.published !== 'number') {
+            return '`publish_date` should be a number.';
+        }
     };
 
     return Backbone.Model.extend(model);
